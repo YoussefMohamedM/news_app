1b3a1ef1559375057f40d0b7eb3e4bcb4d4cb222
YoussefMohamedM <youssefmohamed9525@gmail.com>
2025-09-14 10:55:02 +0300

    add colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkGrey = Color(0xFF171717);
+  static const Color black = Color(0xFF000000);
+  static const Color nearBlack = Color(0xFF121212);
+  static const Color lightGrey = Color(0xFFA0A0A0);
+  static const Color mediumGrey = Color(0xFF808080);
+  static const Color paleGrey = Color(0xFFD9D9D9);
+}
